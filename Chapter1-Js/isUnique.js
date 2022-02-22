24ec71c70d32595d7fb20acf304ffb31fcecbd42
Kenny Krosky <kenny.krosky@gmail.com>
2022-02-22 12:55:46 -0800

    Update isUnique.js

--- Chapter1-Js/isUnique.js
+++ Chapter1-Js/isUnique.js
@@ -1,9 +1,15 @@
 // Chapter 1 is UniqueImplement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures. 
 
+
+// Your current approach is checking the same string so it will always return false.
+// Set the initial value of j to i + 1;
+// also, you need to check against the characters. This method iterates through "Gregg" and not "G", "r", "e" etc.
+// use string.charAt(index)
+
 let allUniqueChars = function (string) {
  for (let i = 0; i < string.length; i++) {
-  for (let j = 0; j < string.length; j++) {
-   if (string[i] === string[j]) {
+  for (let j = i + 1; j < string.length; j++) {
+   if (string.charAt([i]) === string.charAt([j])) { 
     return false; // if match, return flase
    }
   }
